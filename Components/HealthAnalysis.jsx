2981b5637884fa2e6a706f3b81ac01ed6091204b
Kumar Adarsh <kradarsh24jan@gmail.com>
2026-01-22 17:34:31 +0530

    fixed AI analysis issue

--- Components/HealthAnalysis.jsx
+++ Components/HealthAnalysis.jsx
@@ -1,4 +1,4 @@
-import { Card, Loader } from "@mantine/core";
+import { Button, Card, Group, Loader,Divider,Flex,Title, Badge } from "@mantine/core";
 import api from "../src/api/axios";
 import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
@@ -8,35 +8,52 @@ const HealthAnalysis = () => {
     return localStorage.getItem("savedAnalysis");
   });
   const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => {
+    const getHealthAnalysis = async () => {
+      try {
+        console.log("YES I RAN FROM GET HEALTH ANALYSIS!");
 
-  const getHealthAnalysis = async () => {
-    try {
-      setIsLoading(true);
-      const response = await api.post(`/api/vitals/analysis`);
-      localStorage.setItem("savedAnalysis", response.data.analysis);
-      console.log("Here is your health analysis!");
-
-      // console.log(response.data.analysis);
-      setAnalysis(response.data.analysis);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
+        setIsLoading(true);
+        const response = await api.post(`/api/vitals/analysis`);
+        localStorage.setItem("savedAnalysis", response.data.analysis);
+        console.log("Here is your health analysis!");
+        // console.log(response.data.analysis);
+        setAnalysis(response.data.analysis);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getHealthAnalysis();
   };
 
-  useEffect(() => {
-    console.log(" I RAN on refresh!");
+  // useEffect(() => {
+  //   console.log(" I RAN on refresh!");
 
-    const handler = () => getHealthAnalysis();
-    vitalsEventBus.addEventListener("vitals-updated", handler);
-    return () => {
-      vitalsEventBus.removeEventListener("vitals-updated", handler);
-    };
-  }, []);
+  //   const handler = () => getHealthAnalysis();
+  //   vitalsEventBus.addEventListener("vitals-updated", handler);
+  //   return () => {
+  //     vitalsEventBus.removeEventListener("vitals-updated", handler);
+  //   };
+  // }, []);
 
   return (
     <Card shadow="lg" padding="lg" radius="lg" withBorder>
+      <Flex align="center" justify="space-between">
+        {/* <Badge color="blue">status</Badge> */}
+        <Title order={4}>AI Based Analysis</Title>
+        <Button
+          size="sm"
+          onClick={handleClick}
+          variant="gradient"
+          gradient={{ from: "blue", to: "cyan", deg: 90 }}
+        >
+          Get/Regenerate Analysis
+        </Button>
+      </Flex>
+      <Divider my="lg" />
+
       {isLoading ? <Loader color="indigo" /> : <Markdown>{analysis}</Markdown>}
     </Card>
   );
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,13 @@
 import { Button } from "@mantine/core";
 import { Link } from "react-router-dom";
 import "./dashboard.css";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 
 const Dashboard = () => {
+  const setIsAuthenticated = useContext(AuthContext).setIsAuthenticated;
+  localStorage.removeItem("token");
+  setIsAuthenticated(false);
   return (
     <div className="landing-page">
       {/* Navbar */}
